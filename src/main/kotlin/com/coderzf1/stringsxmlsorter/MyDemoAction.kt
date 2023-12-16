604e5987f37181d56cc8424ea06fd7c11d1a11c9
Johannes Bester <bester.jpb@gmail.com>
2023-12-16 08:18:53 +0200

    Force Background Thread (BGT)

--- src/main/kotlin/com/coderzf1/stringsxmlsorter/MyDemoAction.kt
+++ src/main/kotlin/com/coderzf1/stringsxmlsorter/MyDemoAction.kt
@@ -1,6 +1,7 @@
 package com.coderzf1.stringsxmlsorter
 
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
@@ -8,7 +9,6 @@ import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
 import org.simpleframework.xml.Attribute
 import org.simpleframework.xml.ElementList
@@ -16,125 +16,132 @@ import org.simpleframework.xml.Root
 import org.simpleframework.xml.Text
 import org.simpleframework.xml.core.Persister
 import java.awt.BorderLayout
-import java.io.OutputStreamWriter
 import java.io.StringWriter
 import javax.swing.JComponent
 import javax.swing.JPanel
 
-class MyDemoAction:AnAction() {
+class MyDemoAction : AnAction() {
 
-    override fun update(e: AnActionEvent) {
-        val currentFile:VirtualFile? = e.getData(PlatformDataKeys.VIRTUAL_FILE)
-        val filename = currentFile?.name
-        if (filename != "strings.xml"){
-            e.presentation.isEnabled = false
-        }
-        super.update(e)
-    }
 
-    override fun actionPerformed(e: AnActionEvent) {
-        FileDocumentManager.getInstance().saveAllDocuments()
-        val sortDialog = SortDialogWrapper()
+	override fun getActionUpdateThread(): ActionUpdateThread {
+//		return super.getActionUpdateThread()
+		return ActionUpdateThread.BGT  // Force Background Thread (BGT)
+	}
 
-        if (sortDialog.showAndGet()){
-            when(sortDialog.sortBy){
-                "Value"->{
-                    sortByValue(e)
-                }
-                "Name"->{
-                    sortByName(e)
-                }
-            }
-        }
-    }
+	override fun update(e: AnActionEvent) {
+		val currentFile: VirtualFile? = e.getData(PlatformDataKeys.VIRTUAL_FILE)
+		val filename = currentFile?.name
+		if (filename != "strings.xml") {
+			e.presentation.isEnabled = false
+		}
+		super.update(e)
+	}
 
-    private fun sortByValue(e: AnActionEvent){
-        val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
-        val document = editor.document
-        val serializer = Persister()
-        val resources = serializer.read(Resources::class.java, document.text)
-        var sortedResources = resources.xmlStringList?.sortedWith(
-            compareBy {
-                it.value?.lowercase()
-            }
-        )
-        val writer = StringWriter()
-        resources.xmlStringList = sortedResources
-        serializer.write(resources,writer)
-        WriteCommandAction.runWriteCommandAction(editor.project) {
-            document.setText(writer.toString())
-        }.run {
+	override fun actionPerformed(e: AnActionEvent) {
+		FileDocumentManager.getInstance().saveAllDocuments()
+		val sortDialog = SortDialogWrapper()
 
-        }
-        FileDocumentManager.getInstance().saveAllDocuments()
-    }
+		if (sortDialog.showAndGet()) {
+			when (sortDialog.sortBy) {
+				"Value" -> {
+					sortByValue(e)
+				}
 
-    private fun sortByName(e: AnActionEvent){
-        val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
-        val document = editor.document
-        val serializer = Persister()
-        val resources = serializer.read(Resources::class.java, document.text)
-        var sortedResources = resources.xmlStringList?.sortedWith(
-            compareBy {
-                it.name?.lowercase()
-            }
-        )
-        val writer = StringWriter()
-        resources.xmlStringList = sortedResources
-        serializer.write(resources,writer)
-        WriteCommandAction.runWriteCommandAction(editor.project) {
-            document.setText(writer.toString())
-        }.run {
+				"Name" -> {
+					sortByName(e)
+				}
+			}
+		}
+	}
 
-        }
-        FileDocumentManager.getInstance().saveAllDocuments()
-    }
+	private fun sortByValue(e: AnActionEvent) {
+		val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
+		val document = editor.document
+		val serializer = Persister()
+		val resources = serializer.read(Resources::class.java, document.text)
+		val sortedResources = resources.xmlStringList?.sortedWith(
+			compareBy {
+				it.value?.lowercase()
+			}
+		)
+		val writer = StringWriter()
+		resources.xmlStringList = sortedResources
+		serializer.write(resources, writer)
+		WriteCommandAction.runWriteCommandAction(editor.project) {
+			document.setText(writer.toString())
+		}.run {
+
+		}
+		FileDocumentManager.getInstance().saveAllDocuments()
+	}
+
+	private fun sortByName(e: AnActionEvent) {
+		val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
+		val document = editor.document
+		val serializer = Persister()
+		val resources = serializer.read(Resources::class.java, document.text)
+		val sortedResources = resources.xmlStringList?.sortedWith(
+			compareBy {
+				it.name?.lowercase()
+			}
+		)
+		val writer = StringWriter()
+		resources.xmlStringList = sortedResources
+		serializer.write(resources, writer)
+		WriteCommandAction.runWriteCommandAction(editor.project) {
+			document.setText(writer.toString())
+		}.run {
+
+		}
+		FileDocumentManager.getInstance().saveAllDocuments()
+	}
 }
 
-class SortDialogWrapper:DialogWrapper(true){
+class SortDialogWrapper : DialogWrapper(true) {
 
-    var sortBy: String = "Name"
+	var sortBy: String = "Name"
 
-    init {
-        title = "Sort By"
-        init()
-    }
-    override fun createCenterPanel(): JComponent? {
-        val dialogPanel = JPanel(BorderLayout())
-        val combo = ComboBox<String>()
-        combo.addItem("Name")
-        combo.addItem("Value")
-        combo.addActionListener {
-            if(it.actionCommand == "comboBoxChanged"){
-                sortBy = combo.selectedItem?.toString()?:"Value"
-            }
-        }
-        dialogPanel.add(combo, BorderLayout.CENTER)
-        return dialogPanel
-    }
+	init {
+		title = "Sort By"
+		init()
+	}
+
+	override fun createCenterPanel(): JComponent {
+		val dialogPanel = JPanel(BorderLayout())
+		val combo = ComboBox<String>()
+		combo.addItem("Name")
+		combo.addItem("Value")
+		combo.addActionListener {
+			if (it.actionCommand == "comboBoxChanged") {
+				sortBy = combo.selectedItem?.toString() ?: "Value"
+			}
+		}
+		dialogPanel.add(combo, BorderLayout.CENTER)
+		return dialogPanel
+	}
 
 }
 
-@Root(name="string", strict = false)
-class XmlString{
-    @set:Text(required = false)
-    @get:Text(required = false)
-    var value:String? = null
+@Root(name = "string", strict = false)
+class XmlString {
+	@set:Text(required = false)
+	@get:Text(required = false)
+	var value: String? = null
 
-    @set:Attribute(required = false, name = "name")
-    @get:Attribute(required = false, name = "name")
-    var name:String? = null
+	@set:Attribute(required = false, name = "name")
+	@get:Attribute(required = false, name = "name")
+	var name: String? = null
 
-    override fun toString(): String {
-        return "$name : $value"
-    }
+	override fun toString(): String {
+		return "$name : $value"
+	}
 }
 
 @Root(name = "resources", strict = false)
 class Resources {
-    @get:ElementList(entry = "string", name = "resources", required = false, inline = true)
-    @set:ElementList(entry = "string", name = "resources", required = false, inline = true)
-    var xmlStringList: List<XmlString>? = null
+	@get:ElementList(entry = "string", name = "resources", required = false, inline = true)
+	@set:ElementList(entry = "string", name = "resources", required = false, inline = true)
+	var xmlStringList: List<XmlString>? = null
 }
 
 //<resources>
