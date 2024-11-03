da23c6b62150dd00c0904c7cc65df01585fadc06
Carl Williams <williams.carl.83@gmail.com>
2024-11-03 12:40:26 -0500

    -Updated plugin for Android Studio LadyBug -Increased Version Code to Snapshot 4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,65 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.21"
-    id("org.jetbrains.intellij") version "1.13.0"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0-Beta2"
+    //id("org.jetbrains.intellij") version "1.17.4"
 }
 
+
+
 group = "com.coderzf1"
-version = "1.0-SNAPSHOT"
+version = "4.0-SNAPSHOT" //Reminder, Increase Version Number Here for each update
 
 repositories {
     mavenCentral()
+    intellijPlatform{
+        defaultRepositories()
+    }
 }
 dependencies {
     implementation("org.simpleframework:simple-xml:2.7.1")
+    intellijPlatform{
+        androidStudio("2024.2.2.9")
+        bundledPlugin("org.jetbrains.android")
+        pluginVerifier()
+        zipSigner()
+        instrumentationTools()
+    }
 }
 
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.1.1.26")
-    type.set("AI") // Target IDE Platform
+// Android Studio Releases for IntelliJ Plugins: https://plugins.jetbrains.com/docs/intellij/android-studio-releases-list.html
+//
+//intellij {
+//    version.set("2024.2.2.9")
+//    type.set("AI") // Target IDE Platform
+//    plugins.set(listOf("android","org.jetbrains.android"))
+//}
 
-    plugins.set(listOf("android"))
-}
+
 
 tasks {
     buildSearchableOptions {
         enabled = false
     }
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "21"
     }
 
+    patchPluginXml{
+        sinceBuild.set("231")
+        untilBuild.set("243.*")
+    }
 
-    runIde { ideDir.set( file("C:\\Program Files\\Android\\Android Studio2") )}
+    //runIde { ideDir.set( file("C:\\Program Files\\Android\\Android Studio2") )}
 
-
+    publishPlugin {
+        token.set("perm:d2lsbGlhbXMuY2FybC44Mw==.OTItOTI3MQ==.Lw5wKUtuT0VsbWH36uW7h7GS5bP92l")
+    }
 }
